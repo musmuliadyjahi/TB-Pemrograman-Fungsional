2079c6991969bcd80ac229734fb2f0d125bf7498
Akmal Dzaky <akmaldzaky33@gmail.com>
2019-11-04 23:17:37 +0700

    penambahan jawaban modul 3 nomor 2 untuk bahan jawaban modul 4 nomor 2

--- Project/Praktikum/Modul 4/Modul 4.py
+++ Project/Praktikum/Modul 4/Modul 4.py
@@ -10,17 +10,50 @@
 # Angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # angkaI(Angka)
 # -------------------------------------------------------------------------------------------------------
-def cube(x,y):
+def cube(x, y):
     return y**x
+
 
 def my_map(method, argument_list):
     print("Angka = ", argument_list)
-    for x in range(2,6):
+    for x in range(2, 6):
         arr = []
         for y in argument_list:
-            arr.append(method(x,y))
-        print("Hasil Pangkat",x, "=",arr)
+            arr.append(method(x, y))
+        print("Hasil Pangkat", x, "=", arr)
     return arr
+
+
 my_list = my_map(cube, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 my_list
+
+# ==========================================================================================================
+buku = [
+    {'judul': 'anoboy', 'harga': 1, 'pengarang': 'wahyu',
+        'kutipan': 'Gratis itu baik'},
+    {'judul': 'naruto', 'harga': 2, 'pengarang': 'william',
+        'kutipan': 'Bar-bar adalah jalan ninjaku'},
+    {'judul': 'Big Hero', 'harga': 3, 'pengarang': 'angel',
+        'kutipan': 'Saya saja bisa menjadi hero, kenapa kamu tidak'},
+]
+
+
+def cari(buku, subsring):
+    return buku['judul'] in substring['judul']\
+        and buku['harga'] in substring['harga']\
+        and buku['pengarang'] in substring['pengarang']\
+        and buku['kutipan'] in substring['kutipan']
+
+
+def tampil(key, value):
+    if value == True:
+        print(key, value)
+
+
+print("Nomor 2")
+asd = input("Masukkan judul buku : ")
+substring = {'judul': asd, 'harga': list(
+    range(1, 5)), 'pengarang': 'wahyu, william, angel', 'kutipan': 'Gratis itu baik,Bar-bar adalah jalan ninjaku,Saya saja bisa menjadi hero, kenapa kamu tidak'}
+x = list(map(cari, buku, [substring]*len(buku)))
+list(map(tampil, buku, x))
