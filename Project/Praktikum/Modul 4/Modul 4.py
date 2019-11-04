2e2b9819c11294680a2dc5c283fd0401142ae209
Akmal Dzaky <akmaldzaky33@gmail.com>
2019-11-04 21:20:05 +0700

    final nomor 1

--- Project/Praktikum/Modul 4/Modul 4.py
+++ Project/Praktikum/Modul 4/Modul 4.py
@@ -1,10 +1,26 @@
-def angkaI(list):
-    for i in range(2,6):
-        arr = []
-        for j in range(1,len(list)+1):
-            temp = j**i
-            arr.append(temp)
-        print(arr)
+# def angkaI(list):
+#     for i in range(2, 6):
+#         arr = []
+#         for j in range(1, len(list)+1):
+#             temp = j**i
+#             arr.append(temp)
+#         print(arr)
 
-Angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-angkaI(Angka)
+
+# Angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# angkaI(Angka)
+# -------------------------------------------------------------------------------------------------------
+def cube(x,y):
+    return x**y
+
+def my_map(method, argument_list):
+    print("Angka = ", argument_list)
+    for x in range(2,6):
+        arr = []
+        for y in argument_list:
+            arr.append(method(y,x))
+        print("Hasil Pangkat",x, "=",arr)
+    return arr
+my_list = my_map(cube, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+my_list
