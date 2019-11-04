43e9fef228867e06b16188bce0eceae1bde7ce3d
Akmal Dzaky <akmaldzaky33@gmail.com>
2019-11-04 21:22:00 +0700

    perubahan x dan y agar tidak bingung

--- Project/Praktikum/Modul 4/Modul 4.py
+++ Project/Praktikum/Modul 4/Modul 4.py
@@ -11,14 +11,14 @@
 # angkaI(Angka)
 # -------------------------------------------------------------------------------------------------------
 def cube(x,y):
-    return x**y
+    return y**x
 
 def my_map(method, argument_list):
     print("Angka = ", argument_list)
     for x in range(2,6):
         arr = []
         for y in argument_list:
-            arr.append(method(y,x))
+            arr.append(method(x,y))
         print("Hasil Pangkat",x, "=",arr)
     return arr
 my_list = my_map(cube, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
