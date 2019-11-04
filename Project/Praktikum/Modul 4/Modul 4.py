f36a2cc08b2c284dee04a7b6b1e0d3c0edbaac02
Akmal Dzaky <akmaldzaky33@gmail.com>
2019-11-04 23:33:07 +0700

    check point, lanjut pagi

--- Project/Praktikum/Modul 4/Modul 4.py
+++ Project/Praktikum/Modul 4/Modul 4.py
@@ -30,30 +30,40 @@ my_list
 
 # ==========================================================================================================
 buku = [
-    {'judul': 'anoboy', 'harga': 1, 'pengarang': 'wahyu',
-        'kutipan': 'Gratis itu baik'},
-    {'judul': 'naruto', 'harga': 2, 'pengarang': 'william',
-        'kutipan': 'Bar-bar adalah jalan ninjaku'},
-    {'judul': 'Big Hero', 'harga': 3, 'pengarang': 'angel',
-        'kutipan': 'Saya saja bisa menjadi hero, kenapa kamu tidak'},
+    {'judul': 'anoboy', 'harga': 1, 'pengarang': 'wahyu'},
+    {'judul': 'naruto', 'harga': 2, 'pengarang': 'william'},
+    {'judul': 'Big Hero', 'harga': 3, 'pengarang': 'angel'},
+]
+arr = [
+    {'kutipan': 'Gratis itu baik'}, 
+    {'kutipan': 'Bar-bar adalah jalan ninjaku'}, 
+    {'kutipan': 'Saya saja bisa menjadi hero, kenapa kamu tidak'},
 ]
 
 
-def cari(buku, subsring):
+def kutipan(arr, substring):
+    arr = arr
+
+    def cetak():
+        print(arr['kutipan'])
+
+    return cetak
+
+
+def cari(buku, substring):
     return buku['judul'] in substring['judul']\
         and buku['harga'] in substring['harga']\
-        and buku['pengarang'] in substring['pengarang']\
-        and buku['kutipan'] in substring['kutipan']
+        and buku['pengarang'] in substring['pengarang']
 
 
 def tampil(key, value):
     if value == True:
-        print(key, value)
+        print(key)
 
 
 print("Nomor 2")
 asd = input("Masukkan judul buku : ")
 substring = {'judul': asd, 'harga': list(
-    range(1, 5)), 'pengarang': 'wahyu, william, angel', 'kutipan': 'Gratis itu baik,Bar-bar adalah jalan ninjaku,Saya saja bisa menjadi hero, kenapa kamu tidak'}
+    range(1, 5)), 'pengarang': 'wahyu, william, angel'}
 x = list(map(cari, buku, [substring]*len(buku)))
 list(map(tampil, buku, x))
