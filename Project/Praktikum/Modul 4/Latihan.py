a666934857becf878af82e5315257da6ccdb23ea
Akmal Dzaky <akmaldzaky33@gmail.com>
2019-11-10 09:55:41 +0700

    a

--- Project/Praktikum/Modul 4/Latihan.py
+++ Project/Praktikum/Modul 4/Latihan.py
@@ -1,35 +1,44 @@
 # 1
-def tambah(x,y):
+def tambah(x, y):
     return x+y
 
-print(tambah(2,4))
+
+print(tambah(2, 4))
 
 lainnya = tambah
-print(lainnya(5,5))
+print(lainnya(5, 5))
 # 2
+
+
 def potong(kata):
     return kata[10:17]
 
+
 def menyapa(kata):
     return kata.replace("Hallo", "Hei")
+
 
 def eksekusi(func):
     greeting = func("Hallo dunia dan Hallo praktikum Informatika")
     print(greeting)
 
+
 eksekusi(potong)
 eksekusi(menyapa)
 # 3
+
+
 def angkaI(a):
     def angkaII(b):
         return a*b
-    
+
     return angkaII
+
 
 dikali = angkaI(12)
 print(dikali(2))
 # 4
-List = ['aa','bb',35,dikali]
+List = ['aa', 'bb', 35, dikali]
 List.append(dikali(2))
 print(List)
 # 5
@@ -38,17 +47,20 @@ print(List)
 
 #     def bFunction():
 #         print(text)
-    
+
 #     bFunction()
 # aFunction('contoh Nested Function')
 # 6
+
+
 def aFunction(text):
-    text = text
+    Text = text
 
     def bFunction():
-        print(text)
-    
+        print(Text)
+
     return bFunction
+
 
 myFunction = aFunction('Contoh Closure')
 myFunction()
--- Project/Praktikum/Modul 4/Modul 4.py
+++ Project/Praktikum/Modul 4/Modul 4.py
@@ -13,6 +13,7 @@
 def cube(x, y):
     return y**x
 
+print("Nomor 1")
 
 def my_map(method, argument_list):
     print("Angka = ", argument_list)
@@ -23,47 +24,45 @@ def my_map(method, argument_list):
         print("Hasil Pangkat", x, "=", arr)
     return arr
 
-
 my_list = my_map(cube, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-
 my_list
 
 # ==========================================================================================================
 buku = [
-    {'judul': 'anoboy', 'harga': 1, 'pengarang': 'wahyu'},
-    {'judul': 'naruto', 'harga': 2, 'pengarang': 'william'},
-    {'judul': 'Big Hero', 'harga': 3, 'pengarang': 'angel'},
-]
-arr = [
-    {'kutipan': 'Gratis itu baik'}, 
-    {'kutipan': 'Bar-bar adalah jalan ninjaku'}, 
-    {'kutipan': 'Saya saja bisa menjadi hero, kenapa kamu tidak'},
+    {'judul': 'anoboy', 'harga': 1, 'pengarang': 'wahyu',
+        'kutipan': 'Gratis itu baik'},
+    {'judul': 'naruto', 'harga': 2, 'pengarang': 'william',
+        'kutipan': 'Bar-bar adalah jalan ninjaku'},
+    {'judul': 'Big Hero', 'harga': 3, 'pengarang': 'angel',
+        'kutipan': 'Saya saja bisa menjadi hero, kenapa kamu tidak'},
 ]
 
-
-def kutipan(arr, substring):
-    arr = arr
+def kutipan(buku, judul):
+    Buku = buku
+    Judul = judul
 
     def cetak():
-        print(arr['kutipan'])
-
+        for x in range(len(buku)):
+            if Buku[x]['judul'] == Judul:
+                asd = Buku[x]['kutipan']
+                print("Kutipan :", asd)
     return cetak
-
 
 def cari(buku, substring):
     return buku['judul'] in substring['judul']\
         and buku['harga'] in substring['harga']\
-        and buku['pengarang'] in substring['pengarang']
-
+        and buku['pengarang'] in substring['pengarang']\
+        and buku['kutipan'] in substring['kutipan']
 
 def tampil(key, value):
     if value == True:
-        print(key)
-
+        print("Judul buku :", key['judul'])
 
 print("Nomor 2")
 asd = input("Masukkan judul buku : ")
 substring = {'judul': asd, 'harga': list(
-    range(1, 5)), 'pengarang': 'wahyu, william, angel'}
+    range(1, 5)), 'pengarang': 'wahyu, william, angel', 'kutipan': 'Gratis itu baik, Bar-bar adalah jalan ninjaku, Saya saja bisa menjadi hero, kenapa kamu tidak'}
 x = list(map(cari, buku, [substring]*len(buku)))
 list(map(tampil, buku, x))
+ss = kutipan(buku, substring['judul'])
+ss()
