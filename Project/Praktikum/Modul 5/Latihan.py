8c71a627ad4c9d36027cc3734f42a6fa43bfef3d
Akmal Dzaky <akmaldzaky33@gmail.com>
2019-11-19 13:30:13 +0700

    menambah comment dan nomor 4

--- Project/Praktikum/Modul 5/Latihan.py
+++ Project/Praktikum/Modul 5/Latihan.py
@@ -1,14 +1,20 @@
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+x = [0, 1, 3]
+y = [0, 2, 3]
 
-friends = [70, 65, 72, 63, 71, 64, 60, 64, 67]
-minutes = [175, 170, 205, 120, 220, 130, 105, 145, 190]
-labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
-plt.scatter(friends, minutes)
-# label each point
-for label, friend_count, minute_count in zip(labels, friends, minutes):
-    plt.annotate(label, xy=(friend_count, minute_count),
-                 xytext=(5, -5), textcoords='offset points')
-plt.title("Daily Minutes vs. Number of Friends")
-plt.xlabel("# of friends")
-plt.ylabel("daily minutes spent on the site")
+# create a line chart, years on x-axis, gdp on y-axis
+plt.arrow(x[0], y[0], 1, 2, head_width=0.1,
+          head_length=0.2, length_includes_head=True)
+plt.arrow(x[1], y[1], 2, 1, head_width=0.1,
+          head_length=0.2, length_includes_head=True)
+plt.arrow(x[2], y[2], -x[2], -y[2])
+
+# add a title
+plt.title("Chart Garis")
+# add a label to the y-axis
+plt.ylim(-0.5, 3.5)
+plt.xlim(-1, 4)
+plt.xlabel("x axis")
+plt.ylabel("y axis")
+
 plt.show()
---
+++ Project/Praktikum/Modul 5/Modul5-Nomor4.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt  # Memanggil extension matplotlib
+
+# membuat koordinat dari segitiga
+x = [0, 1, 3]
+y = [0, 2, 3]
+dx = [1, 2, -3]
+dy = [2, 1, -3]
+
+# membuat segitiga
+for i in range(len(x)-1):
+    plt.arrow(x[i], y[i], dx[i], dy[i], head_width=0.1,
+          head_length=0.2, length_includes_head=True) #membuat garis dengan kepala panah
+    
+plt.arrow(x[2], y[2], dx[2], dy[2]) #Membuat koordinat dan garis tanpa panah
+# Menambahkan title
+plt.title("Chart Garis")
+# mengubah limit sumbu x dan y
+plt.ylim(-0.5, 3.5)
+plt.xlim(-1, 4)
+#memberi nama sumbu x dan y
+plt.xlabel("Sumbu x")
+plt.ylabel("Sumbu y")
+#menampilkn tabel
+plt.show()
