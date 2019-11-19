6322f607d49a5dfe37e570ec7e87f48d7466532d
Akmal Dzaky <akmaldzaky33@gmail.com>
2019-11-19 10:45:19 +0700

    menambah comment

--- Project/Praktikum/Modul 5/Modul5-Nomor2.py
+++ Project/Praktikum/Modul 5/Modul5-Nomor2.py
@@ -1,19 +1,23 @@
+#Memanggil extension matplotlib dan string, string digunakan untuk membuat alfabet
 import matplotlib.pyplot as plt
 import string
 
-alpha = string.ascii_lowercase
-y = [83, 95, 91, 87, 70, 0, 85, 82, 100, 67, 73, 77, 0]
-x, y1, y2, y3 = [], [], [], []
+#Menyiapkan variabel
+alpha = string.ascii_lowercase #Isi variabel adalah alfabet dari extension string
+y = [83, 95, 91, 87, 70, 0, 85, 82, 100, 67, 73, 77, 0] #nilai asli
+#membuat list kosong untuk di isi dengan alfabet dan hasil perhitungan nilai sumbu Y
+x, y1, y2, y3 = [], [], [], [] 
 
+#membuat perulangan sesuai dengan anggota y
 for i in range(len(y)):
-    y1.append(y[i]*1.5)
-    y2.append(y[i]*-1)
-    y3.append(y[i]*-1.5)
-    x.append(alpha[i])
+    y1.append(y[i]*1.5) #membuat anggota y1 = 1.5y
+    y2.append(y[i]*-1) #membuat anggota y1 = -1y
+    y3.append(y[i]*-1.5) #membuat anggota y1 = -1.5y
+    x.append(alpha[i]) #manggabungkan alfabet ke list x
 
-plt.plot(x, y, linestyle='-.')
-plt.plot(x, y1, linestyle=':')
-plt.plot(x, y2, linestyle='--')
-plt.plot(x, y3, linestyle='-')
-plt.grid(True)
-plt.show()
+plt.plot(x, y, linestyle='-.') #plot 1 dengan style -.
+plt.plot(x, y1, linestyle=':') #plot 2 dengan style :
+plt.plot(x, y2, linestyle='--') #plot 3 dengan style --
+plt.plot(x, y3, linestyle='-') #plot 4 dengan style garis
+plt.grid(True) #membuat garis pada kotak
+plt.show() #menampilkan tabel
--- Project/Praktikum/Modul 5/Modul5-Nomor3.py
+++ Project/Praktikum/Modul 5/Modul5-Nomor3.py
@@ -1,16 +1,20 @@
+#Memanggil extension matplotlib dan string, string digunakan untuk membuat alfabet
 import matplotlib.pyplot as plt
 import string
 
-alpha = string.ascii_lowercase
-y = [83, 95, 91, 87, 70, 0, 85, 82, 100, 67, 73, 77, 0]
+#Menyiapkan variabel
+alpha = string.ascii_lowercase #Isi variabel adalah alfabet dari extension string
+y = [83, 95, 91, 87, 70, 0, 85, 82, 100, 67, 73, 77, 0] #nilai asli
+#membuat list kosong untuk di isi dengan alfabet dan hasil perhitungan nilai sumbu Y
 x, y1, y2 = [], [], []
 
+#membuat perulangan sesuai dengan anggota y
 for i in range(len(y)):
-    y1.append(y[i]*1.5)
-    y2.append(y[i]*-1)
-    x.append(alpha[i])
+    y1.append(y[i]*1.5) #membuat anggota y1 = 1.5y
+    y2.append(y[i]*-1) #membuat anggota y1 = -1y
+    x.append(alpha[i]) #manggabungkan alfabet ke list x
 
-plt.scatter(x, y, s=100,  marker='^')
-plt.scatter(x, y1, s=150, marker='1')
-plt.scatter(x, y2, s=200, marker='p')
-plt.show()
+plt.scatter(x, y, s=100,  marker='^') #plot 1 dengan model titik segitiga
+plt.scatter(x, y1, s=150, marker='1') #plot 2 dengan model titik 'tiga garis'
+plt.scatter(x, y2, s=200, marker='p') #plot 3 dengan model titik segi-5
+plt.show() #menampilkan tabel
