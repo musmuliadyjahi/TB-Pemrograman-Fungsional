47a1a01c1c010682098fd3823a6aab659eb7053d
Akmal Dzaky <akmaldzaky33@gmail.com>
2019-12-04 11:52:30 +0700

    Modul 6

--- Project/Praktikum/Modul 6/Modul6-Nomor3.py
+++ Project/Praktikum/Modul 6/Modul6-Nomor3.py
@@ -1,12 +1,16 @@
 import skimage.io as io
 from copy import deepcopy
 import matplotlib.pyplot as plt
+from skimage import color
 
 img = io.imread(r'./Project/Praktikum/Modul 6/a.jpg')
 
 red = deepcopy(img)
+print(type(red))
 green = deepcopy(img)
 blue = deepcopy(img)
+grey = io.imread(r'./Project/Praktikum/Modul 6/a.jpg', as_gray=True)
+a = color.gray2rgb(grey)
 
 red[:, :, 1] = 0
 red[:, :, 2] = 0
@@ -15,7 +19,7 @@ green[:, :, 2] = 0
 blue[:, :, 0] = 0
 blue[:, :, 1] = 0
 
-fig, ax = plt.subplots(ncols=2, nrows=2)
+fig, ax = plt.subplots(ncols=3, nrows=2)
 
 ax[0, 0].imshow(img)
 ax[0, 0].set_title('ori')
@@ -29,5 +33,9 @@ ax[1, 0].axis('off')
 ax[1, 1].imshow(blue)
 ax[1, 1].set_title('blue')
 ax[1, 1].axis('off')
+ax[0, 2].imshow(a)
+ax[0, 2].set_title('grey')
+ax[0, 2].axis('off')
+ax[1, 2].axis('off')
 
 plt.show()
