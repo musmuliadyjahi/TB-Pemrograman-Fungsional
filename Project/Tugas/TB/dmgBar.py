dd9945f740688613fb93b1b14d5e9805d2366afc
Akmal Dzaky <akmaldzaky33@gmail.com>
2019-12-07 11:35:01 +0700

    Pembuatan bar total dmg

---
+++ Project/Tugas/TB/dmgBar.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+def doffy(dmgList):
+    asd = []
+    for i in range(len(dmgList)):
+        i += 1
+        asd.append(i)
+    plt.bar(asd, dmgList)
+    plt.show()
+
+def bigMom(dmgList):
+    asd = []
+    for i in range(len(dmgList)):
+        i += 1
+        asd.append(i)
+    plt.bar(asd, dmgList)
+    plt.show()
+
+def pacifista(dmgList):
+    asd = []
+    for i in range(len(dmgList)):
+        i += 1
+        asd.append(i)
+    plt.bar(asd, dmgList)
+    plt.show()
