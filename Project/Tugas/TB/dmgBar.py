19ab9b8243b058e7323c043dc2310e8fe9616ead
Akmal Dzaky <akmaldzaky33@gmail.com>
2019-12-07 17:39:58 +0700

    penambahan box crocodile dan kizaru

--- Project/Tugas/TB/dmgBar.py
+++ Project/Tugas/TB/dmgBar.py
@@ -32,3 +32,25 @@ def pacifista(dmgList):
     li = str(dmgList)
     plt.figtext(.02, .02, "Total dmg = " + total +"\n"+ li)
     plt.show()
+
+def crocodile(dmgList):
+    asd = []
+    for i in range(len(dmgList)):
+        i += 1
+        asd.append(i)
+    plt.bar(asd, dmgList)
+    total = str(sum(dmgList))
+    li = str(dmgList)
+    plt.figtext(.02, .02, "Total dmg = " + total +"\n"+ li)
+    plt.show()
+
+def kizaru(dmgList):
+    asd = []
+    for i in range(len(dmgList)):
+        i += 1
+        asd.append(i)
+    plt.bar(asd, dmgList)
+    total = str(sum(dmgList))
+    li = str(dmgList)
+    plt.figtext(.02, .02, "Total dmg = " + total +"\n"+ li)
+    plt.show()
